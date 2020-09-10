77d81b8f5440228317ad56886e071750d5d2fc83
bense <bense@gmail.com>
2020-09-10 11:30:32 +0800

    http client trace and ecode

--- net/http/blademaster/server.go
+++ net/http/blademaster/server.go
@@ -5,8 +5,18 @@ import "github.com/gin-gonic/gin"
 type ServerConfig struct {
 }
 
-func DefaultServer(conf *ServerConfig) *gin.Engine {
+func New() *gin.Engine {
+	engine := gin.New()
+	return engine
+}
+
+func Default() *gin.Engine {
 	engine := gin.Default()
+	return engine
+}
+
+func DefaultServer(conf *ServerConfig) *gin.Engine {
+	engine := gin.New()
 	engine.Use(Trace())
 	return engine
 }
