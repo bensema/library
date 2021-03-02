531d9d6ae89f175bd64c678de5f48d545ec105ca
bense <bense@gmail.com>
2021-03-02 17:12:18 +0800

    crypto

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## cache
     redis 缓存
 
+## crypto
+    AES RSA
---
+++ crypto/example/aes.go
@@ -0,0 +1,38 @@
+/*
+CBC/ECB/CFB
+*/
+package main
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"github.com/bensema/library/crypto"
+	"log"
+)
+
+func main() {
+	origData := []byte("Hello World") // 待加密的数据
+	key := []byte("ABCDEFGHIJKLMNOP") // 加密的密钥
+	log.Println("原文：", string(origData))
+
+	log.Println("------------------ CBC模式 --------------------")
+	encrypted := crypto.AesEncryptCBC(origData, key)
+	log.Println("密文(hex)：", hex.EncodeToString(encrypted))
+	log.Println("密文(base64)：", base64.StdEncoding.EncodeToString(encrypted))
+	decrypted := crypto.AesDecryptCBC(encrypted, key)
+	log.Println("解密结果：", string(decrypted))
+
+	log.Println("------------------ ECB模式 --------------------")
+	encrypted = crypto.AesEncryptECB(origData, key)
+	log.Println("密文(hex)：", hex.EncodeToString(encrypted))
+	log.Println("密文(base64)：", base64.StdEncoding.EncodeToString(encrypted))
+	decrypted = crypto.AesDecryptECB(encrypted, key)
+	log.Println("解密结果：", string(decrypted))
+
+	log.Println("------------------ CFB模式 --------------------")
+	encrypted = crypto.AesEncryptCFB(origData, key)
+	log.Println("密文(hex)：", hex.EncodeToString(encrypted))
+	log.Println("密文(base64)：", base64.StdEncoding.EncodeToString(encrypted))
+	decrypted = crypto.AesDecryptCFB(encrypted, key)
+	log.Println("解密结果：", string(decrypted))
+}
