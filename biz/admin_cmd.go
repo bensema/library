177791ddee0e7dca84f22316b8618ed624b91ec1
bense <bense@gmail.com>
2021-03-26 11:20:28 +0800

    cmd

--- biz/admin_cmd.go
+++ biz/admin_cmd.go
@@ -1,5 +1,17 @@
 package biz
 
+type AdminRequest struct {
+	Operator   string   `json:"operator"`
+	OperatorId string   `json:"operator_id"`
+	Token      string   `json:"token"`
+	Auth       string   `json:"auth"`
+	Ip         string   `json:"ip"`
+	T          int64    `json:"t"`
+	R          string   `json:"r"`
+	Cmd        AdminCmd `json:"cmd"`
+	Data       string   `json:"data"`
+}
+
 type AdminCmd int
 
 const (
