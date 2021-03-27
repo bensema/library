8945875202ccbd1770e121240418f2afc929a6be
bense <bense@gmail.com>
2021-03-27 15:52:50 +0800

    cmd

--- biz/admin_cmd.go
+++ biz/admin_cmd.go
@@ -27,4 +27,115 @@ const (
 	AnnouncementUpdate
 	AnnouncementQuery
 	AnnouncementPages
+	GameAdd
+	GameDel
+	GameUpdate
+	GameQuery
+	GamePages
+
+	GameTypeAdd
+	GameTypeDel
+	GameTypeUpdate
+	GameTypeQuery
+	GameTypePages
+	GameTypeFind
+	GameTypeAll
+
+	GameGroupAdd
+	GameGroupDel
+	GameGroupUpdate
+	GameGroupQuery
+	GameGroupPages
+	GameGroupFind
+	GameGroupAll
+
+	CoinAdd
+	CoinDel
+	CoinUpdate
+	CoinQuery
+	CoinPages
+	CoinFind
+	CoinAll
+
+	CoinChainAdd
+	CoinChainDel
+	CoinChainUpdate
+	CoinChainQuery
+	CoinChainPages
+	CoinChainFind
+	CoinChainAll
+
+	GradeAdd
+	GradeDel
+	GradeUpdate
+	GradeQuery
+	GradePages
+	GradeFind
+	GradeAll
+
+	LogOperateAdd
+	LogOperateDel
+	LogOperateUpdate
+	LogOperateQuery
+	LogOperatePages
+
+	LogLoginAdd
+	LogLoginDel
+	LogLoginUpdate
+	LogLoginQuery
+	LogLoginPages
+
+	LogUserLoginAdd
+	LogUserLoginDel
+	LogUserLoginUpdate
+	LogUserLoginQuery
+	LogUserLoginPages
+
+	PowerAdd
+	PowerDel
+	PowerUpdate
+	PowerQuery
+	PowerPages
+
+	SecurityQuestionsAdd
+	SecurityQuestionsDel
+	SecurityQuestionsUpdate
+	SecurityQuestionsQuery
+	SecurityQuestionsPages
+
+	UserInfoAdd
+	UserInfoDel
+	UserInfoUpdate
+	UserInfoQuery
+	UserInfoPages
+
+	UserPowerAdd
+	UserPowerDel
+	UserPowerUpdate
+	UserPowerQuery
+	UserPowerPages
+
+	UserSecurityQuestionAdd
+	UserSecurityQuestionDel
+	UserSecurityQuestionUpdate
+	UserSecurityQuestionQuery
+	UserSecurityQuestionPages
+
+	UserTokenAdd
+	UserTokenDel
+	UserTokenUpdate
+	UserTokenQuery
+	UserTokenPages
+
+	UserWalletAdd
+	UserWalletDel
+	UserWalletUpdate
+	UserWalletQuery
+	UserWalletPages
+
+	UserAdd
+	UserDel
+	UserUpdate
+	UserQuery
+	UserPages
 )
